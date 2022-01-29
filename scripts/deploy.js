c204e8d314ab3dbf0e7719fc63ceb5fae294b43c
felix0888 <felixnan88@gmail.com>
2022-01-28 19:50:05 -0500

    Elevator contract

---
+++ scripts/deploy.js
@@ -0,0 +1,23 @@
+const hre = require("hardhat");
+
+async function main() {
+  const [deployer] = await ethers.getSigners();
+
+  console.log(
+    "Deploying contracts with the account: ", deployer.address
+  );
+
+  console.log("Account balance: ", (await deployer.getBalance()).toString());
+
+  const Elevator = await ethers.getContractFactory("Elevator");
+  const elevator = await Elevator.deploy();
+  console.log("Elevator address: ", await elevator.address);
+  console.log("Account balance after Elevator deploy: ", (await deployer.getBalance()).toString());
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch(error => {
+    console.error(error);
+    process.exit(1);
+  });
